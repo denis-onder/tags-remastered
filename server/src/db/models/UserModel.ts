e3af16e76b762cae4247c123fb915c4786b7b105
Denis Onder <denis_onder@protonmail.com>
2021-11-22 21:59:04 +0100

    Hashing passwords

--- server/src/db/models/UserModel.ts
+++ server/src/db/models/UserModel.ts
@@ -1,5 +1,6 @@
-import User from '../../domain/User';
+import argon2 from 'argon2';
 import { model, Schema } from 'mongoose';
+import User from '../../domain/User';
 
 const schema: Schema<User> = new Schema({
   id: {
@@ -18,6 +19,13 @@ const schema: Schema<User> = new Schema({
     type: String,
     required: true,
   },
+});
+
+schema.pre('save', async function (next) {
+  const user: User = this;
+  user.password = await argon2.hash(user.password);
+
+  next();
 });
 
 export default model<User>('user', schema);
